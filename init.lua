5cf76551c9b2c096d78f72e75dd07a38a8823c8b
AgapovOne <agapov.one@gmail.com>
2024-06-18 10:32:01 +0500

    Add vimcmd, log highlight

--- init.lua
+++ init.lua
@@ -2,5 +2,9 @@
 require("config.lazy")
 
 vim.cmd([[autocmd BufRead,BufNewFile *.podspec set filetype=ruby]])
+vim.cmd([[autocmd BufRead,BufNewFile Podfile set filetype=ruby]])
 vim.opt.spelllang = { "en", "ru" }
 vim.opt.conceallevel = 0
+
+vim.opt.langmap =
+  "ФИСВУАПРШОЛДЬТЩЗЙКЫЕГМЦЧНЯ;ABCDEFGHIJKLMNOPQRSTUVWXYZ,фисвуапршолдьтщзйкыегмцчня;abcdefghijklmnopqrstuvwxyz"
